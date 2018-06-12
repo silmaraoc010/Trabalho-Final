bfc3fe26ae7357da5a3f6fd2dd20e1d1458a51fe
silmaraoc010 <39534832+silmaraoc010@users.noreply.github.com>
2018-06-12 13:50:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ for(var i = 0; i<phrase.length; i++){
 
 alert (textCipher);
 
-//decipher to ficando louca.
+//decipher
 var phrase = prompt('Digite uma frase para criptografar')
 if(phrase !== '' && phrase.replace(/[^0-9]/g, ' ') !== phrase){
 // se o texto for diferente de 0, ou se você inserir qualquer valor numérico, ele o substituirá por vazio e chama o prompt novamente
@@ -53,52 +53,3 @@ function decipher(textCipher){
   return encrypted;
 }}
 
-
-// var phrase = prompt('Digite uma frase para criptografar')
-// if(phrase !== '' && phrase.replace(/[^0-9]/g, ' ') !== phrase){
-// // se o texto for diferente de 0, ou se você inserir qualquer valor numérico, ele o substituirá por vazio e chama o prompt novamente
-//   cipher(phrase); // chamamos a função para executar
-
-// }
-// else{
-//   alert('Digite uma frase'); 
-// // Se não for digitado nenhuma frase  o prompt será chamado novamente
-//   var phrase = prompt('Digite uma frase para criptografar');
-// }  
-
-// //function Cipher
-// function cipher(phrase){
-//   //Guarda as letras de um novo texto
-//   var textCipher = "";
-//  // passando pelo texto digitado
-//   for(var i = 0; i<phrase.length; i++){
- 
-// // descobrindo o código ASCII das letras e aplicar a fórmula
-//    var textChar = (phrase.charCodeAt(i) - 65 + 33)%26 + 65;
-
-// // convertendo o texto cifrado 
-//    textCipher += String.fromCharCode(textChar);
-//  }
-//  //retorna o valor
-
- // return textCipher;
- document.getElementById("CipherPhrase").innerHTML += textCipher;
-// }
-
-  
-
-  
-// //funcion Decipher
-
-// //   //tratando de invertir la function cipher
-// // function DecipherPhrase(phrase){
-// //   //para guardar las letras de la letra a descifrar
-// //   var textDecipher = "";
-// //   //
-// //     for(var i = 0; i<phrase.length; i++){
-// //       var textChar = (String.fromCharCode(phrase(i)) + 33)%26;
-// //       textDecipher = textDecipher + textChar;
-// //       var newText = newText.charCodeAt(textDecipher);
-// //     }
-// //     return newText;
-// // }
